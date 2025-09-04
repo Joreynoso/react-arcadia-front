1d2a3aa29ce737a9145884faba049a7376001508
Joreynoso <joreynoso.developer@gmail.com>
2025-09-04 17:47:39 -0300

    donde section cards

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -7,14 +7,14 @@ export default function CardsSection() {
 
     // statics games info
     const games = [
-        { id: 1, name: 'name game', released: 'date released', background_image: 'http:/&' },
-        { id: 2, name: 'name game', released: 'date released', background_image: 'http:/&' },
-        { id: 3, name: 'name game', released: 'date released', background_image: 'http:/&' },
-        { id: 4, name: 'name game', released: 'date released', background_image: 'http:/&' },
-        { id: 5, name: 'name game', released: 'date released', background_image: 'http:/&' },
-        { id: 6, name: 'name game', released: 'date released', background_image: 'http:/&' },
-        { id: 7, name: 'name game', released: 'date released', background_image: 'http:/&' },
-        { id: 8, name: 'name game', released: 'date released', background_image: 'http:/&' },
+        { id: 1, name: 'The Elder Scrolls: Arena', released: '1994-02-28', background_image: 'https://media.rawg.io/media/games/ef8/ef823f3c46ff1e00803888b5c4476e10.jpg' },
+        { id: 2, name: 'Tomb Raider II', released: '1997-10-31', background_image: 'https://media.rawg.io/media/games/cfc/cfc91eccc1de338b75a0576310ccbcec.jpg' },
+        { id: 3, name: 'name game', released: 'date released', background_image: 'https://media.rawg.io/media/games/6c0/6c00ee85d1344f58c469e8e47fd8ae7c.jpg' },
+        { id: 4, name: 'Resident Evil 1', released: '1996-03-22', background_image: 'https://media.rawg.io/media/games/510/51039d0ec5dc8c3e08ae4374dfceecec.jpg' },
+        { id: 5, name: 'Final Fantasy III', released: '1994-04-02', background_image: 'https://media.rawg.io/media/games/98c/98c87b286cd2a2ba942167df384a9bd3.jpg' },
+        { id: 6, name: 'Doom', released: '1993-12-10', background_image: 'https://media.rawg.io/media/games/47b/47b50d880be8453bf9cda6e5c007bc26.jpg' },
+        { id: 7, name: 'Fallout', released: '1997-09-30', background_image: 'https://media.rawg.io/media/games/14a/14a83c56ff668baaced6e8c8704b6391.jpg' },
+        { id: 8, name: 'STAR WARS Jedi Knight', released: '1998-02-17', background_image: 'https://media.rawg.io/media/screenshots/3ee/3ee387f0e1681ec4c7f082ce371ce904.jpg' },
     ]
 
     const gameList = games.map(game => (
@@ -26,14 +26,17 @@ export default function CardsSection() {
             key={game.id}
             className="relative rounded-xl bg-card flex flex-col p-2 md:p-3 border-arcadia gap-2 md:gap-3 h-auto md:h-72 text-center" >
             <div className="relative rounded-lg h-40 md:h-48 bg-[#FCCE9F] border-arcadia overflow-hidden">
-                <GameNotImage />
+                <img
+                    src={game.background_image}
+                    className="w-full h-full object-cover"
+                />
             </div>
 
             {/* info */}
             <div className="flex flex-col text-center px-1 md:px-2" >
                 <h4 className="text-sm md:text-base text-arcadia truncate font-semibold">{game.name}</h4>
                 <span className="text-[10px] md:text-xs text-arcadia opacity-70 font-semibold">
-                    {game.released}
+                   Released: {game.released}
                 </span>
             </div>
 
